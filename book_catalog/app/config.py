cf8a07eee217f7b7d8f6c3530fa58956478ca654
hongphuc218 <hongphuc218@gmail.com>
2024-09-14 00:57:53 +1000

    updated

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://s224806968_postgresql_ubus_user:PBGp4wBfZMslGy5oyACtETlbXm0SNZYs@dpg-crertn2j1k6c73dfr76g-a.singapore-postgres.render.com/s224806968_postgresql_ubus')
--- inventory_management/app/schemas.py
+++ inventory_management/app/schemas.py
@@ -11,4 +11,4 @@ class Inventory(InventoryBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
